36fb0599d7c7801c54b8d31fa38bc93c055e6204
Ali Sherkat Ghanad <sherkata714@gmail.com>
2024-01-29 01:08:27 +0330

    update the js file

--- FAQs-Project/script.js
+++ FAQs-Project/script.js
@@ -1,11 +1,20 @@
 const content = document.querySelectorAll('.q-box');
+const plusIcon = document.querySelectorAll('.plus-icon');
+const minusIcon = document.querySelectorAll('.minus-icon');
 
-content.forEach((element) => {
+content.forEach((element , index) => {
   element.addEventListener("click", () => {
     const dropDown = element.nextElementSibling;
-    const isDropDownVisible = window.getComputedStyle(dropDown).display != 'none';
-
-    dropDown.style.display = isDropDownVisible ? 'none' : 'block';
+    const isDropDownVisible = window.getComputedStyle(dropDown).display == 'none';
+    dropDown.style.display = isDropDownVisible ? 'block' : 'none';
+  
+    if (isDropDownVisible) {
+      plusIcon[index].style.display = 'none';
+      minusIcon[index].style.display = 'block';
+    } else {
+      plusIcon[index].style.display = 'block';
+      minusIcon[index].style.display = 'none';
+    }
   });
 });
 
